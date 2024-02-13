ba3fafd4ecee6d66aeb8b2fc52a65afce5ea57d5
AACarvalho20232026 <145613828+AACarvalho20232026@users.noreply.github.com>
2024-02-12 23:39:34 -0100

    Create part3.cpp

---
+++ part3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    // Declare uma variável para armazenar o número fornecido pelo usuário.
+    int n;
+
+    // Leia um número inteiro n do utilizador.
+    cout << "Digite um número inteiro: ";
+    cin >> n;
+
+    // Utilize um loop for para calcular e exibir a tabuada de n (de 1 a 10).
+    cout << "Tabuada de " << n << ":\n";
+    for (int i = 1; i <= 10; ++i) {
+        cout << n << " x " << i << " = " << (n * i) << "\n";
+    }
+
+    return 0;
+}
